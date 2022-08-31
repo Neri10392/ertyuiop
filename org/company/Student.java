4c9a7bcd8f775cbcc02e84613f97d3df8658d529
Neri10392 <neriyarasan@gmail.com>
2022-08-31 22:35:36 +0530

    Inherit

---
+++ org/company/Student.java
@@ -0,0 +1,27 @@
+package org.company;
+
+public class Student {
+	public void studentName() {
+         System.out.println("Name is Neri");
+	}
+	public void studentDept() {
+      System.out.println("EEE Department");
+	}
+	public void studentId() {
+       System.out.println("2345678");
+	}
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.studentName();
+		s.studentDept();
+		s.studentId();
+		College c = new College();
+		
+		
+		
+		
+		
+	}
+	
+}
+	
